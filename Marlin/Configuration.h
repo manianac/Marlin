06cada43f993933fa1b8211b736692b70fc28797
Manianac <2092573+manianac@users.noreply.github.com>
2022-04-02 14:40:44 -0400

    Updated VREF var for other boards Enabled fast_pwm for fans (noisy otherwise)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1179,7 +1179,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 44, -0.5, -2.96 }
+#define NOZZLE_TO_PROBE_OFFSET { 44, -0.5, -2.87 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
